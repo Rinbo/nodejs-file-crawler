bcd76290fa698cd5226c95f2e31c3259de35bde1
Rinbo <robin.b@outlook.com>
2019-03-17 16:30:42 +0100

    Add reader, lister and updater

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 const lib = {};
 
-lib.baseDir = path.join(__dirname, "./data");
+lib.baseDir = path.join(__dirname, "./");
+lib.searchDir = path.join(__dirname, "../");
 
 // Write data to file
 lib.create = function(dir, file, data, callback) {
@@ -30,3 +31,95 @@ lib.create = function(dir, file, data, callback) {
     }
   });
 };
+
+lib.read = function(dir, file, callback) {
+  fs.readFile(
+    `${lib.searchDir}/${dir == null ? "" : dir + "/"}${file}`,
+    "utf8",
+    function(err, data) {
+      if (!err && data) {
+        //const parsedData = helpers.parseJsonToObject(data);
+        callback(false, data);
+      } else callback(err, data);
+    }
+  );
+};
+
+// Update a file with new data
+lib.update = function(dir, file, data, callback) {
+  // Open file for writing
+  fs.open(`${lib.baseDir}/${dir}/${file}.txt`, "r+", function(
+    err,
+    fileDesciptor
+  ) {
+    if (!err && fileDesciptor) {
+      // Convert data to string
+      const stringData = JSON.stringify(data);
+
+      // Truncate the file
+      fs.ftruncate(fileDesciptor, function(err) {
+        if (!err) {
+          fs.writeFile(fileDesciptor, data, function(err) {
+            if (!err) {
+              fs.close(fileDesciptor, function(err) {
+                if (!err) {
+                  callback(false);
+                } else {
+                  callback("Error closing the file");
+                }
+              });
+            } else {
+              callback("Could not write to file");
+            }
+          });
+        } else {
+          callback("Error truncating the file");
+        }
+      });
+    } else {
+      callback("Could not open the file for updating, it may not exist");
+    }
+  });
+};
+
+lib.listDirectoryContent = function(dir, callback) {
+  fs.readdir(
+    `${lib.searchDir}/${dir == null ? "" : dir + "/"}`,
+    { encoding: "utf8", withFileTypes: true },
+    function(err, data) {
+      if (!err && data && data.length > 0) {
+        const content = [];
+        data.forEach(element => {
+          if (
+            element != "" &&
+            element.name != ".git" &&
+            element.name != ".gitignore" &&
+            element.name != "node_modules" &&
+            element.name != "bundle.js"
+          )
+            content.push(element);
+        });
+        callback(false, content);
+      } else {
+        callback(err, data);
+      }
+    }
+  );
+};
+
+// Not needed
+lib.listFiles = function(dir, callback) {
+  fs.readdir(`${lib.baseDir}/${dir}/`, function(err, data) {
+    if (!err && data && data.length > 0) {
+      const trimmedFileNames = [];
+      data.forEach(fileName => {
+        trimmedFileNames.push(fileName.replace(".json", ""));
+      });
+      callback(false, trimmedFileNames);
+    } else {
+      callback(err, data);
+    }
+  });
+};
+
+module.exports = lib;
--- index.js
+++ index.js
@@ -1,2 +1,67 @@
-import data from "./data";
+const fileSystem = require("./data");
 
+// Begin reading from directory one step above project root
+fileSystem.listDirectoryContent(null, (err, dirContentL1) => {
+  if (!err) {
+    // Take the directory contents and loop through it to see if it contains folders or files
+    dirContentL1.forEach(elementL1 => {
+      if (elementL1.isFile()) {
+        // Element is a file
+        if (elementL1.name.match(".html")) {
+          fileSystem.read(null, elementL1.name, (err, data) => {
+            if (!err) {
+              // Loop through each line in file and append it to output file if the line fullfills certain criteria
+              data.split("\n").forEach(line => {
+                console.log(line)
+                fileSystem.update("data", "output", line+"\n", err => {
+                  if (!err) {
+                    console.log("Success");
+                  } else {
+                    console.log(err, "Bummer!");
+                  }
+                });
+              });
+            } else {
+              console.log(err, "unable to read from file [level 1]");
+            }
+          });
+        }
+      } else {
+        // Element is a folder
+        fileSystem.listDirectoryContent(elementL1.name, (err, dirContentL2) => {
+          if (!err) {
+            dirContentL2.forEach(elementL2 => {
+              if (elementL2.isFile()) {
+                // Element is a file
+              } else {
+                // Element is a folder
+                fileSystem.listDirectoryContent(
+                  elementL1.name + "/" + elementL2.name,
+                  (err, dirContentL3) => {
+                    if (!err) {
+                      dirContentL3.forEach(elementL3 => {
+                        if (elementL3.isFile()) {
+                        } else {
+                          //console.log("Maximum depth level reached")
+                        }
+                      });
+                    } else {
+                      console.log(
+                        err,
+                        "Unable to read from directory [Level3]"
+                      );
+                    }
+                  }
+                );
+              }
+            });
+          } else {
+            console.log(err, "Unable to read from directory [level 2]");
+          }
+        });
+      }
+    });
+  } else {
+    console.log(err, "Unable to read from directory [level 1]");
+  }
+});
